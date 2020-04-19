26bb57d8593561298ea2015f2b32b739e02809c9
Jacob Soo <jacobsoo@users.noreply.github.com>
2020-04-19 14:45:46 +0800

    Update SandroRat.py

--- amtracker/core/SandroRat.py
+++ amtracker/core/SandroRat.py
@@ -1,5 +1,6 @@
 import re, os, zlib, base64
 from typing import List
+import struct
 from androguard.core.bytecodes import apk
 from androguard.core.bytecodes import dvm
 from amtracker.common.out import _log
@@ -43,41 +44,44 @@ class SandroRat(object):
     # extract_config : This extracts the C&C information from SandroRat.
     #-----------------------------------------------------------------
     def extract_config(self, apkfile):
-        self.apkfile = apkfile
-        string = ""
-        bIndicate = False
-        a = apk.APK(self.apkfile)
-        d = dvm.DalvikVMFormat(a.get_dex())
-        for cls in d.get_classes():
-            if 'Lnet/droidjack/server/MainActivity;'.lower() in cls.get_name().lower():
-                for method in cls.get_methods():
-                    if 'onCreate'.lower() in str(method).lower():
-                        for inst in method.get_instructions():
-                            if inst.get_name() == 'sget-byte':
-                                string = inst.get_output().split(',')[-1].strip(" '")
-                                string, szMet = string.split("->")
-                                bIndicate = True
-                                break
-        if bIndicate:
+        try:
+            self.apkfile = apkfile
+            string = ""
+            bIndicate = False
+            a = apk.APK(self.apkfile)
+            d = dvm.DalvikVMFormat(a.get_dex())
             for cls in d.get_classes():
-                if string.lower() in cls.get_name().lower():
-                    c2 = ""
-                    port = ""
-                    szTemp = None
+                if 'Lnet/droidjack/server/MainActivity;'.lower() in cls.get_name().lower():
                     for method in cls.get_methods():
-                        if '<clinit>'.lower() in str(method).lower():
+                        if 'onCreate'.lower() in str(method).lower():
                             for inst in method.get_instructions():
-                                if inst.get_name() == 'const-string':
-                                    c2 = inst.get_output().split(',')[-1].strip(" '")
-                                if inst.get_name() == 'const/16':
-                                    port = inst.get_output().split(',')[-1].strip(" '")
-                                if c2 and port:
+                                if inst.get_name() == 'sget-byte':
+                                    string = inst.get_output().split(',')[-1].strip(" '")
+                                    string, szMet = string.split("->")
+                                    bIndicate = True
                                     break
-                    server = ""
-                    if port:
-                        server = "{0}:{1}".format(c2, str(port))
-                    else:
-                        server = c2
-                    _log('[+] Extracting from %s' % self.apkfile)
-                    _log('[+] C&C: [ %s ]\n' % server)
-                    return True
+            if bIndicate:
+                for cls in d.get_classes():
+                    if string.lower() in cls.get_name().lower():
+                        c2 = ""
+                        port = ""
+                        szTemp = None
+                        for method in cls.get_methods():
+                            if '<clinit>'.lower() in str(method).lower():
+                                for inst in method.get_instructions():
+                                    if inst.get_name() == 'const-string':
+                                        c2 = inst.get_output().split(',')[-1].strip(" '")
+                                    if inst.get_name() == 'const/16':
+                                        port = inst.get_output().split(',')[-1].strip(" '")
+                                    if c2 and port:
+                                        break
+                        server = ""
+                        if port:
+                            server = "{0}:{1}".format(c2, str(port))
+                        else:
+                            server = c2
+                        _log('[+] Extracting from %s' % self.apkfile)
+                        _log('[+] C&C: [ %s ]\n' % server)
+                        return True
+        except struct.error:
+            _log("[-] Possibly corrupted APK")
