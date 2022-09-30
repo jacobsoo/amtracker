62d4029ad45a534b06ca5768800e290a06eb9f88
Jacob Soo <jacobsoo@users.noreply.github.com>
2022-09-30 11:15:22 +0800

    Add files via upload

--- amtracker/core/AhMyth.py
+++ amtracker/core/AhMyth.py
@@ -3,14 +3,16 @@ from typing import List
 from androguard.core.bytecodes import apk
 from androguard.core.bytecodes import dvm
 from amtracker.common.out import _log
+from androguard.core.bytecodes.dvm import DalvikVMFormat
+from androguard.core.analysis import analysis
 
 '''
     Hashes for samples:
+    d4e16801c46f51f704ed439fe7648e9d93a2b8f571d7120657f64190f6028b23
     ab99a33e7528b6b95c03e51336ad7fd54442722f08a3634b96dead3a091a6da1
     8007346a57fbe2965b6a58b4a2d7bb21e8230fb642707409bb91d5c1010a9f80
     8fad8429b4e0ed5c2ed6dffec4989fd6861cf7afa47695e7d53bb0cc3196e1f8
     d15648a84b46f97c93f59b0b5b09d8b0572f972292bfda31eb3864f412d86d51
-    fad306dd9f1bdd6c132a074446947ac7a57e42df975cb440a26e051ee4cf6b99
 '''
 
 class AhMyth(object):
@@ -28,10 +30,16 @@ class AhMyth(object):
     def verifyAhMyth(self, apkfile):
         self.apkfile = apkfile
         a = apk.APK(self.apkfile)
-        szPackageName = a.get_package()
-        if "ahmyth.mine.king.ahmyth" in szPackageName:
-            bRes = self.extract_config(self.apkfile)
-            return bRes
+        # szPackageName = a.get_package()
+        # if "ahmyth.mine.king.ahmyth" in szPackageName:
+        d = dvm.DalvikVMFormat(a.get_dex())
+        for cls in d.get_classes():
+            if 'ahmyth/mine/king/ahmyth/'.lower() in cls.get_name().lower():
+                _log("[+] This is AhMyth")
+                bRes = self.extract_config(self.apkfile)
+                if bRes == None:
+                    _log("[-] This apk likely uses Multi-Dex")
+                return bRes
         else:
             _log("[-] This is not AhMyth")
 
